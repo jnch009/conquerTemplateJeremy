18f8604de8e163b81252f7bb7f37855fedb704e7
jnch009 <jngchenghin@gmail.com>
2019-11-16 13:57:59 -0800

    flex-flow for parallax text

--- src/components/services.js
+++ src/components/services.js
@@ -4,17 +4,19 @@ import "./services.css";
 function Services() {
   return (
     <div id="services">
-      <div class="parallax parallax--bg ">
-        <h1>Our Services</h1>
-        <p>
-          Nunc diam leo, fringilla vulputate elit lobortis, consectetur
-          vestibulum quam. Sed id
-          <br />
-          felis ligula. In euismod libero at magna dapibus, in rutrum velit
-          lacinia.
-          <br />
-          Etiam a mi quis arcu varius condimentum.
-        </p>
+      <div class="parallaxWrapper">
+        <div class="parallax parallax--bg parallax__flex">
+          <h1>Our Services</h1>
+          <p>
+            Nunc diam leo, fringilla vulputate elit lobortis, consectetur
+            vestibulum quam. Sed id
+            <br />
+            felis ligula. In euismod libero at magna dapibus, in rutrum velit
+            lacinia.
+            <br />
+            Etiam a mi quis arcu varius condimentum.
+          </p>
+        </div>
       </div>
     </div>
   );
