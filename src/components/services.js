1e9dd484f8d0a7447b2781af11742396c0427584
jnch009 <jngchenghin@gmail.com>
2019-11-16 13:16:06 -0800

    Setting up HTML for services

---
+++ src/components/services.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div id="services">
+      <div class="parallax bg">
+        <h1>Our Services</h1>
+        <p>
+          Nunc diam leo, fringilla vulputate elit lobortis, consectetur
+          vestibulum quam. Sed id
+          <br />
+          felis ligula. In euismod libero at magna dapibus, in rutrum velit
+          lacinia.
+          <br />
+          Etiam a mi quis arcu varius condimentum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
