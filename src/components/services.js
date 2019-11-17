58bdb347a66e78a19d9852cb5447b5d4bd19609f
jnch009 <jngchenghin@gmail.com>
2019-11-16 21:16:30 -0800

    Updating font styling of services text

--- src/components/services.js
+++ src/components/services.js
@@ -1,23 +1,58 @@
 import React from "react";
 import "./services.css";
+import Card from "./card";
 
 function Services() {
   return (
     <div id="services">
-      <div class="parallaxWrapper">
-        <div class="parallax parallax--bg parallax__flex">
-          <h1>Our Services</h1>
-          <p>
-            Nunc diam leo, fringilla vulputate elit lobortis, consectetur
-            vestibulum quam. Sed id
-            <br />
-            felis ligula. In euismod libero at magna dapibus, in rutrum velit
-            lacinia.
-            <br />
-            Etiam a mi quis arcu varius condimentum.
-          </p>
-        </div>
+      <div class="parallax">
+        <p class="testHeader">Our Services</p>
+        <p class="testParagraph">
+          Nunc diam leo, fringilla vulputate elit lobortis, consectetur
+          vestibulum quam. Sed id
+          <br />
+          felis ligula. In euismod libero at magna dapibus, in rutrum velit
+          lacinia.
+          <br />
+          Etiam a mi quis arcu varius condimentum.
+        </p>
       </div>
+      <Card
+        title="Bootstrap v3.3.6"
+        titleStyle="titleTopRow"
+        body="Morbi sagittis justo a velit placerat ullamcorper quis quis
+            velit. Sed convallis at risus ullamcorper auctor. Praesent quis
+            velit neque. Quisque semper porta nisi vitae suscipit. Duis
+            lectus magna, ornare ac scelerisque."
+        buttonText="Button Green"
+        imgLink="1-1.jpg"
+        buttonClass="topRowButtons"
+        cardColor="green"
+      />
+      <Card
+        title="Bootstrap v3.3.6"
+        titleStyle="titleTopRow"
+        body="Morbi sagittis justo a velit placerat ullamcorper quis quis
+            velit. Sed convallis at risus ullamcorper auctor. Praesent quis
+            velit neque. Quisque semper porta nisi vitae suscipit. Duis
+            lectus magna, ornare ac scelerisque."
+        buttonText="Button Green"
+        imgLink="1-1.jpg"
+        buttonClass="topRowButtons"
+        cardColor="green"
+      />
+      <Card
+        title="Bootstrap v3.3.6"
+        titleStyle="titleTopRow"
+        body="Morbi sagittis justo a velit placerat ullamcorper quis quis
+            velit. Sed convallis at risus ullamcorper auctor. Praesent quis
+            velit neque. Quisque semper porta nisi vitae suscipit. Duis
+            lectus magna, ornare ac scelerisque."
+        buttonText="Button Green"
+        imgLink="1-1.jpg"
+        buttonClass="topRowButtons"
+        cardColor="green"
+      />
     </div>
   );
 }
