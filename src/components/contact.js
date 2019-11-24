544b2cc38d4ef26f5b7c393aa8f3545aa9dc87a8
jnch009 <jngchenghin@gmail.com>
2019-11-23 20:41:58 -0800

    setting up basic html form

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,7 +7,28 @@ class Contact extends React.Component {
       <div id="contact">
         <div class="contactBody">
           <h2>Services</h2>
-          <button class="btn btn--white">Test Button</button>
+          <form method="POST" action="http://localhost:8080" class="btn-center">
+            <input type="text" id="Name" name="Name" placeholder="Name" />
+            <input
+              type="text"
+              id="Address"
+              name="Address"
+              placeholder="Address"
+            />
+            <input
+              type="number"
+              id="Address"
+              name="Address"
+              placeholder="Score"
+            />
+            <button
+              type="submit"
+              class="btn btn--white"
+              onClick={this.handleClick}
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     );
