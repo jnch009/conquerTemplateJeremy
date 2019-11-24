8eba9130cb72ee7b4bf62e916228a74e78bfde2e
jnch009 <jngchenghin@gmail.com>
2019-11-23 21:49:07 -0800

    Updating contact form to allow submission

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,33 +2,88 @@ import React from "react";
 import "./contact.scss";
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", address: "", character: "", tableList: [] };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.baseState = this.state;
+  }
+
+  resetState() {
+    this.setState({ name: "" });
+    this.setState({ address: "" });
+    this.setState({ character: "" });
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleClick(e) {
+    e.preventDefault();
+    const jsonTableEntry = {
+      name: this.state.name,
+      address: this.state.address,
+      character: this.state.character
+    };
+
+    this.setState({ tableList: [...this.state.tableList, jsonTableEntry] });
+    console.log(this.state.tableList);
+    this.resetState();
+  }
+
   render() {
     return (
       <div id="contact">
         <div class="contactBody">
           <h2>Services</h2>
-          <form method="POST" action="http://localhost:8080" class="btn-center">
-            <input type="text" id="Name" name="Name" placeholder="Name" />
+          <form class="btn-center" onSubmit={this.handleClick}>
             <input
               type="text"
-              id="Address"
-              name="Address"
-              placeholder="Address"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={this.handleChange}
             />
             <input
-              type="number"
-              id="Address"
-              name="Address"
-              placeholder="Score"
+              type="text"
+              name="address"
+              placeholder="Address"
+              value={this.state.address}
+              onChange={this.handleChange}
             />
-            <button
-              type="submit"
-              class="btn btn--white"
-              onClick={this.handleClick}
-            >
-              Submit
-            </button>
+            <input
+              type="text"
+              name="character"
+              placeholder="Character"
+              value={this.state.character}
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Submit please" class="btn btn--white" />
           </form>
+        </div>
+        <div class="flex__container">
+          <div class="gridTable">
+            <span>
+              <strong>1</strong>
+            </span>
+            <span>
+              <strong>2</strong>
+            </span>
+            <span>
+              <strong>3</strong>
+            </span>
+            <span>
+              <strong>4</strong>
+            </span>
+            <span>
+              <strong>5</strong>
+            </span>
+          </div>
         </div>
       </div>
     );
